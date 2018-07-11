e295dd374c8237c7fe3479db1744e795a211a8cc
abean <abean@godaddy.com>
2018-07-11 14:54:20 -0700

    initial update for plesk calls in zeus

--- zeus/events/suspension/angelo.py
+++ zeus/events/suspension/angelo.py
@@ -1,15 +1,67 @@
+import json
 import logging
 
-from zeus.events.suspension.interface import Product
+import requests
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning, InsecureRequestWarning
+
+# TODO Uncomment import and add 'Product' back as param in Angelo Class
+# from zeus.events.suspension.interface import Product
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
 
 
-class Angelo(Product):
-    def __init__(self, app_settings):
-        self._logger = logging.getLogger(__name__)
+class Angelo():
 
-    def suspend(self): pass
+    # TODO Add 'app_settings' back as second param in init
+    def __init__(self):
 
-    def reinstate(self): pass
+        # TODO Remove duplicate URL and auth to use settings
+
+        # self.auth = (app_settings.PLESKUSER, app_settings.PLESKPASS)
+        self.auth = ('REDACTED', 'REDACTED')
+
+        # TODO Diablo has an enable FTP option. Find out if plesk has this, and do we want to allow it here too?
+        self.body = json.dumps({"disable_panel": "true", "reason": "DCU Testing", "type": "abuse"}, ensure_ascii=False)
+
+        self.headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+        # self.url = app_settings.PLESK_URL
+        self.url = 'https://p3nwplskapp-v01.shr.prod.phx3.secureserver.net:8084/v1/accounts/'
+
+    def suspend(self, shopper_id, guid):
+        plesk_shopper = shopper_id + '/'
+        flag = '?suspend'
+
+        try:
+            with requests.Session() as session:
+                r = session.post(self.url + plesk_shopper + guid + flag, auth=self.auth, headers=self.headers,
+                                 data=self.body, verify=False)
+            if r.status_code == 200:
+                return True
+            else:
+                logging.error('Failed to suspend GUID: {}, request status code: {}').format(guid, r.status_code)
+
+        except Exception as e:
+            logging.error(e.message)
+        return False
+
+    def reinstate(self, shopper_id, guid):
+        plesk_shopper = shopper_id + '/'
+        flag = '?reinstate'
+
+        try:
+            with requests.Session() as session:
+                r = session.post(self.url + plesk_shopper + guid + flag, auth=self.auth, headers=self.headers,
+                                 data=self.body, verify=False)
+            if r.status_code == 200:
+                return True
+            else:
+                logging.error('Failed to reinstate GUID: {}, request status code: {}').format(guid, r.status_code)
+
+        except Exception as e:
+            logging.error(e.message)
+        return False
 
     def cancel(self): pass
 
