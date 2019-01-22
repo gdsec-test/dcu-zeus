a1e7fe185878034ce73ec880796d8b8d7e106522
Nathan Lemay <nlemay@godaddy.com>
2019-01-22 10:32:07 -0800

    Adds support for pip 18.1 and backwards compatibility for <9.0.3 (#15)

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
-from pip.req import parse_requirements
+with open('requirements.txt') as f:
+    install_reqs = f.read().splitlines()
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
-testing_reqs = parse_requirements('test_requirements.txt', session=False)
+with open('test_requirements.txt') as f:
+    testing_reqs = f.read().splitlines()
 
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
-test_reqs = [str(ir.req) for ir in testing_reqs]
+with open('README.md') as f:
+    long_description = f.read()
 
-setup(name='zeus',
-      version='1.0',
-      description='Deals with automated action of abuse reports',
-      author='DCU',
-      author_email='dcueng@godaddy.com',
-      url='https://github.secureserver.net/nlemay/zeus',
-      package_dir={'zeus': 'zeus'},
-      packages=['zeus', 'zeus.events', 'zeus.events.email', 'zeus.events.support_tools', 'zeus.events.suspension',
-                'zeus.events.user_logging', 'zeus.handlers', 'zeus.persist', 'zeus.reviews', 'zeus.utils'],
-      include_package_data=True,
-      install_requires=reqs,
-      tests_require=test_reqs,
-      test_suite="nose.collector")
+setup(
+    name='zeus',
+    version='1.0',
+    author='DCU',
+    author_email='dcueng@godaddy.com',
+    description='Deals with automated action of abuse reports',
+    long_description=long_description,
+    url='https://github.secureserver.net/nlemay/zeus',
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_reqs,
+    tests_require=testing_reqs,
+    test_suite='nose.collector',
+    classifiers=[
+        'Programming Language :: Python :: 2.7'
+    ]
+)
