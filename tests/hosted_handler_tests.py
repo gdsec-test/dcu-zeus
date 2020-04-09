58de1b4641d5429a81236954da26296a46f82ff6
Jasleen Jabbal <jjabbal@godaddy.com>
2020-04-09 09:47:13 -0700

    Flake8 fixes (#80)

--- tests/hosted_handler_tests.py
+++ tests/hosted_handler_tests.py
@@ -118,8 +118,7 @@ class TestHostedHandler:
     @patch.object(HostedScribe, 'intentionally_malicious', return_value=None)
     @patch.object(ThrottledHostingService, 'can_suspend_hosting_product', return_value=True)
     @patch.object(SSLMailer, 'send_revocation_email', return_value=True)
-    def test_intentionally_malicious_failed_shopper_email_fraud_hold(self, ssl_mailer, can_suspend, scribe, slack, mailer,
-                                                          journal, mimir, shoplocked):
+    def test_intentionally_malicious_failed_shopper_email_fraud_hold(self, ssl_mailer, can_suspend, scribe, slack, mailer, journal, mimir, shoplocked):
         assert_false(self._hosted.intentionally_malicious(self.ticket_fraud_hold))
 
     @patch.object(Shoplocked, 'adminlock', return_value=None)
--- tests/registered_handler_tests.py
+++ tests/registered_handler_tests.py
@@ -98,8 +98,7 @@ class TestRegisteredHandler:
     @patch.object(ThrottledCRM, 'notate_crm_account', return_value=None)
     @patch.object(ThrottledDomainService, 'can_suspend_domain', return_value=True)
     @patch.object(SSLMailer, 'send_revocation_email', return_value=True)
-    def test_intentionally_malicious_failed_shopper_email_fraud_hold(self, ssl_mailer, service, crm, fraud, registered, slack,
-                                                          journal, shoplocked):
+    def test_intentionally_malicious_failed_shopper_email_fraud_hold(self, ssl_mailer, service, crm, fraud, registered, slack, journal, shoplocked):
         assert_false(self._registered.intentionally_malicious(self.ticket_fraud_hold))
 
     @patch.object(Shoplocked, 'adminlock', return_value=None)
