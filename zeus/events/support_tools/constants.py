0e1cd6fb37c918624d4587c2bb80af51f7f40354
Scott S <ssanfratello@godaddy.com>
2021-01-14 07:48:06 -0700

    [CMAPT-4004] Update Permanent Hosted Suspension Notes  (#122)
    * Update permanent suspension notes to clarify do not reinstate/unlock as appropriate, other changes for consistency
    
    * Update for further consistency with Care docs
    
    * Add and reference "DO NOT..." constants for permanent locks and suspensions

--- zeus/events/support_tools/constants.py
+++ zeus/events/support_tools/constants.py
@@ -1,9 +1,11 @@
 # support document created to provide additional resources to Care Agents
-support_doc = "See http://x.co/dcuwhat2do for more info."
+support_doc = "See http://x.co/dcuwhat2do for proper handling."
+no_reinstate = "* DO NOT REINSTATE *"
+no_unlock_reinstate = "* DO NOT UNLOCK OR REINSTATE *"
 
 # SHOPLOCKED CRM notes for Admin Locked accounts
-INTENTIONALLY_MALICIOUS_ADMINLOCK = "Account locked for abuse. Do not unlock the account. {}".format(support_doc)
-SHOPPER_COMPROMISE_ADMINLOCK = "Shopper Account Possibly Compromised. Assist customer with changing password(s) and unlocking account. {}".format(support_doc)
+INTENTIONALLY_MALICIOUS_ADMINLOCK = "Account locked for Abuse. {} {}".format(no_unlock_reinstate, support_doc)
+SHOPPER_COMPROMISE_ADMINLOCK = "Account locked for potential Shopper Compromise. Assist customer with changing password(s), unlocking & securing account. {}".format(support_doc)
 
 note_mappings = {
     'hosted': {
@@ -16,25 +18,25 @@ note_mappings = {
             (support_doc)
         },
         'repeatOffender': {
-            'crm': "Hosting {{guid}} suspended for excessive repeat occurences. {{type}} content reported at {{location}} {}".format(support_doc)
+            'crm': "Hosting {{guid}} suspended for Excessive Repeat Occurrences. {{type}} content reported at {{location}} {}".format(support_doc)
         },
         'suspension': {
-            'crm': "Hosting {{guid}} suspended. {{type}} content still present at {{location}} {}".format(support_doc),
+            'crm': "Hosting {{guid}} suspended. {{type}} Content Still Active After Warning at {{location}} {}".format(support_doc),
             'csam': {
                 'mimir': "Hosted domain {domain} suspended",
-                'crm': "Hosting {{guid}} suspended. Child Abuse material found, do not reinstate any products or account. Any questions and or comments regarding this action reach out to childabuse@"
+                'crm': "Hosting {{guid}} permanently suspended. Child Abuse material found {} Any questions and or comments regarding this action reach out to childabuse@".format(no_unlock_reinstate)
             }
         },
         'intentionallyMalicious': {
-            'crm': "Hosting {{guid}} suspended for intentional {{type}} at {{location}} {}".format(support_doc),
+            'crm': "Hosting {{guid}} permanently suspended for INTENTIONAL {{type}} {} {}".format(no_unlock_reinstate, support_doc),
             'shoplocked': INTENTIONALLY_MALICIOUS_ADMINLOCK
         },
         'shopperCompromise': {
-            'crm': "Hosting {{guid}} suspended for {{type}} at {{location}} as a result of possible shopper account compromise {}".format(support_doc),
+            'crm': "Hosting {{guid}} suspended for {{type}} at {{location}} as a result of possible Shopper Account Compromise {}".format(support_doc),
             'shoplocked': SHOPPER_COMPROMISE_ADMINLOCK
         },
         'extensiveCompromise': {
-            'crm': "Hosting {{guid}} suspended due to extensive compromise. {{type}} content reported at {{location}} {}".format(support_doc)
+            'crm': "Hosting {{guid}} permanently suspended due to Extensive Compromise. {} {}".format(no_reinstate, support_doc)
         },
         'ncmecSubmitted': {
             'mimir': "Report submitted to NCMEC for hosted domain {domain}"
@@ -46,20 +48,20 @@ note_mappings = {
             (support_doc)
         },
         'suspension': {
-            'crm': "{{domain}} suspended. {{type}} content still present at {{location}} {}".format(support_doc),
+            'crm': "{{domain}} suspended. {{type}} Content Still Active After Warning at {{location}} {}".format(support_doc),
             'csam': {
-                'crm': "{{domain}} suspended. Child Abuse material found, do not reinstate any products or account. Any questions and or comments regarding this action reach out to childabuse@"
+                'crm': "{{domain}} permanently suspended. Child Abuse material found {} Any questions and or comments regarding this action reach out to childabuse@".format(no_unlock_reinstate)
             }
         },
         'repeatOffender': {
-            'crm': "{{domain}} suspended for excessive repeat occurences. {{type}} content reported at {{location}} {}".format(support_doc)
+            'crm': "{{domain}} suspended for Excessive Repeat Occurrences. {} {}".format(no_reinstate, support_doc)
         },
         'intentionallyMalicious': {
-            'crm': "{{domain}} suspended for intentional {{type}} at {{location}} {}".format(support_doc),
+            'crm': "{{domain}} permanently suspended for INTENTIONAL {{type}} {} {}".format(no_unlock_reinstate, support_doc),
             'shoplocked': INTENTIONALLY_MALICIOUS_ADMINLOCK
         },
         'shopperCompromise': {
-            'crm': "{{domain}} suspended for {{type}} at {{location}} as a result of possible shopper account compromise {}".format(support_doc),
+            'crm': "{{domain}} suspended for {{type}} at {{location}} as a result of possible Shopper Compromise {}".format(support_doc),
             'shoplocked': SHOPPER_COMPROMISE_ADMINLOCK
         }
     },
@@ -75,10 +77,10 @@ note_mappings = {
 
 alert_mappings = {
     'hosted': {
-        'contentRemoved': "{type} content removed and/or disabled from hosting {domain}. See CRM notes and x.co/dcuwhat2do for more info.",
-        'suspend': "Hosting {domain} suspended for {type} content. See CRM notes and x.co/dcuwhat2do for more info."
+        'contentRemoved': "{type} content removed and/or disabled from hosting {domain}. See CRM notes and x.co/dcuwhat2do for proper handling.",
+        'suspend': "Hosting {domain} suspended for {type} content. See CRM notes and x.co/dcuwhat2do for proper handling."
     },
     'registered': {
-        'suspend': "{domain} suspended for {type} content. See CRM notes and x.co/dcuwhat2do for more info."
+        'suspend': "{domain} suspended for {type} content. See CRM notes and x.co/dcuwhat2do for proper handling."
     }
 }
