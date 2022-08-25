3706e560a170e5312a9dd8613b8ca326cddde65d
htracey-godaddy <htracey@godaddy.com>
2022-08-25 12:49:23 -0700

    adding source

--- run.py
+++ run.py
@@ -179,16 +179,20 @@ def intentionally_malicious(ticket_id, investigator_id):
 @celery.task()
 def suspend(ticket_id):
     data = get_database_handle().get_incident(ticket_id)
+    logger.info(data.get('sourceDomainOrIp', {}))
     result = route_request(data, ticket_id, 'suspend') if data else None
     if result:
         appseclogger = get_logging(os.getenv("sysenv"), "zeus")
         shopper_id = data.get('data', {}).get('domainQuery', {}).get('shopperInfo', {}).get('shopperId')
+        domain = data.get('sourceDomainOrIp', {})
         appseclogger.info("suspending shopper", extra={"event": {"kind": "event",
                                                                  "category": "process",
                                                                  "type": ["change", "user"],
                                                                  "outcome": "success",
                                                                  "action": "suspend"},
-                                                       "shopper_id": shopper_id})
+                                                       "user": {
+                                                           "domain": domain,
+                                                           "shopper_id": shopper_id}})
     return result
 
 
