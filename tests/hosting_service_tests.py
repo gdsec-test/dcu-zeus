4df87295ae2cb9d2d3b4ea06f551484a2ed5973f
Nick Wade <nwade@godaddy.com>
2023-02-14 07:34:23 -0600

    Fix failing unit tests

--- tests/hosting_service_tests.py
+++ tests/hosting_service_tests.py
@@ -8,8 +8,8 @@ from zeus.events.suspension.vps4 import VPS4
 
 
 class TestHostingService(TestCase):
-    DATA_NO_CUSTOMER_ID = {'data': {'domainQuery': {'host': {'shopperId': '1234567'}}}}
-    DATA_WITH_CUSTOMER_ID = {'data': {'domainQuery': {'host': {'customerId': '1234-5678-9012'}}}}
+    DATA_NO_CUSTOMER_ID = {'data': {'domainQuery': {'host': {'shopperId': '1234567', 'entitlementId': 'test-entitlementID'}}}}
+    DATA_WITH_CUSTOMER_ID = {'data': {'domainQuery': {'host': {'customerId': '1234-5678-9012', 'entitlementId': 'test-entitlementID'}}}}
 
     def setUp(self):
         self._hosting_service = HostingService(UnitTestConfig())
@@ -59,20 +59,20 @@ class TestHostingService(TestCase):
     @patch('zeus.events.suspension.nes_helper.NESHelper.get_use_nes', return_value=False)
     @patch('zeus.events.suspension.diablo.Diablo.suspend', return_value=True)
     def test_use_nes_product_false(self, suspend, get_use_nes):
-        self._hosting_service.suspend('diablo', 'test-entitlementID', self.DATA_WITH_CUSTOMER_ID)
-        suspend.assert_called_with(guid='test-entitlementID', data=self.DATA_WITH_CUSTOMER_ID)
+        self._hosting_service.suspend('diablo', 'test-guid', self.DATA_WITH_CUSTOMER_ID)
+        suspend.assert_called_with(guid='test-guid', data=self.DATA_WITH_CUSTOMER_ID)
 
     # TODO CMAPT-5272: next TWO tests - remove the 'get_use_nes' mock value
     @patch('zeus.utils.shopperapi.ShopperAPI.get_customer_id_from_shopper_id', return_value='customerId')
     @patch('zeus.events.suspension.nes_helper.NESHelper.get_use_nes', return_value=True)
     @patch('zeus.events.suspension.nes_helper.NESHelper.suspend', return_value=True)
     def test_use_nes_product_true_no_customer(self, suspend, get_use_nes, get_customer_id_from_shopper):
-        self._hosting_service.suspend("diablo", "test-entitlementID", self.DATA_NO_CUSTOMER_ID)
+        self._hosting_service.suspend('diablo', 'test-guid', self.DATA_NO_CUSTOMER_ID)
         get_customer_id_from_shopper.assert_called_with('1234567')
         suspend.assert_called_with('test-entitlementID', 'customerId')
 
     @patch('zeus.events.suspension.nes_helper.NESHelper.get_use_nes', return_value=True)
     @patch('zeus.events.suspension.nes_helper.NESHelper.suspend', return_value=True)
     def test_use_nes_product_true_customer_exists(self, suspend, get_use_nes):
-        self._hosting_service.suspend("diablo", "test-entitlementID", self.DATA_WITH_CUSTOMER_ID)
+        self._hosting_service.suspend('diablo', 'test-guid', self.DATA_WITH_CUSTOMER_ID)
         suspend.assert_called_with('test-entitlementID', '1234-5678-9012')
